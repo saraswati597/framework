3c4b57a736cdab14cfe24b4b714bf61f6df7f774
saraswati597 <sonijha597@gmail.com>
2019-06-26 23:28:46 +0530

    new commit code

---
+++ selenium/src/selenium/SpecialChar.java
@@ -0,0 +1,14 @@
+package selenium;
+
+public class SpecialChar {
+	public static void main(String[] args) {
+		String s="Asd!dd@gg$gg^&*";
+	char[]ch=	s.toCharArray();
+	//char[]new=new char[ch.length]();
+for(int i=0; i<ch.length;i++)
+{
+	
+}
+	}
+
+}
